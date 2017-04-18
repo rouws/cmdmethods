00e6566a580f279ae36b578477bf1e38d2b54b7d
Sonja Rouwhorst <sonjar@Sonjas-Skinny-MacBook-Pro.local>
2017-04-18 20:31:33 +0200

    versie met alle content en pug bestanden

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-var express = require('express')
+const express = require('express')
+const app = express()
 
-express()
-  .get('/', function (req, res) {
-    res.send('Hello World Justus')
-  })
-  .listen(3000)
+// routers
+const indexRouter = require('./routes/index')
+const categoryRouter = require('./routes/category')
+const cardRouter = require('./routes/card')
+
+
+// view engine setup
+app.set('view engine', 'pug')
+// tell express to use static files in dir 'public'
+app.use(express.static('public'))
+
+//Tell app which routers to use when certain pages are opened
+app.use('/', indexRouter);
+app.use('/:category', categoryRouter);
+app.use('/:category/:card', cardRouter);
+
+// if page cannot be found, render the 404 error page
+app.get('/*', function(req,res){
+    console.log('not found')
+    var err = new Error('Not Found')
+    err.status = 404
+    res.render('error', { error: err})
+});
+
+// start server on port 3000
+app.listen(3000, function () {
+  console.log('Server started on port 3000')
+})
+
