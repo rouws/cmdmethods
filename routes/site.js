7320785da9a44d460d90e02fb8eb1d09545e95eb
Sonja Rouwhorst <sonjar@016.187.028.145.hva.nl>
2017-11-14 10:39:18 +0100

    added favorites

--- routes/site.js
+++ routes/site.js
@@ -1,23 +1,40 @@
 const express = require('express')
 const carddata = require('../carddata')
-
 const router = express.Router()
+const categories = ["library","field","lab","showroom","workshop","stepping-stones"]
+const home = {"strategy" : "home"}
 
 // get the carddata to be displayed
 router.use('/', carddata.shuffledList)
-router.use('/:category', carddata.list)
-router.use('/:category/:cardId', carddata.get)
+router.use('/favorites', carddata.all)
+router.param('category', carddata.categoryList)
+router.param('cardId', carddata.get)
 
 
 // routes
 router.get('/', function (req, res) {
-    res.render('index', { cards: req.cards })
+    res.render('index', { categories: categories,
+                          category: home,
+                          cards: req.cards })
 })
+
+router.get('/favorites', function (req, res) {
+    res.render('favorites', { categories: categories,
+                              category: home,
+                              cards: req.cards })
+})
+
 router.get('/:category', function (req, res) {
-    res.render('category', {category: req.params.category, cards: req.cards})
+    res.render('category', {categories: categories,
+                            category: req.category,
+                            cards: req.cards})
 })
-router.get('/:category/:cardId', function (req, res) {
-    res.render('card-detail', { card: req.card})
+router.get('/cards/:cardId', function (req, res) {
+    res.render('card-detail', { categories: categories,
+                                category: req.category,
+                                card: req.card,
+                                previousCard: req.previousCard,
+                                nextCard: req.nextCard})
 })
 
 module.exports = router;
