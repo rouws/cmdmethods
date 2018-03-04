9064434fa83390352eb47414048406512d9594b1
Sonja Rouwhorst <sonjar@Sonjas-Skinny-MacBook-Pro.local>
2018-03-04 17:44:42 +0100

    andere poort

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.get('*', function(req, res){
 
 
 // start server on port 3000
-app.listen(3000, function () {
+app.listen(5000, function () {
   console.log('=================================Server started on port 3000')
 })
