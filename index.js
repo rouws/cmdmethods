29402bf3972f40833b2e36b5e8fb0cbe5f384cd8
Sonja Rouwhorst <sonjar@Sonjas-Skinny-MacBook-Pro.local>
2018-03-19 20:07:26 +0100

    changed port number

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get('*', function(req, res){
 
 // start server on port 3000
 app.listen(5000, function () {
-  console.log('=================================Server started on port 5000')
+  console.log('=================================Server started on port 3000')
 })
